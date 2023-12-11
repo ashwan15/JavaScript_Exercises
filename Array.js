11e112d99f9f83c9501ec71a6ad67793e8ce13a6
ashwan15 <alal56548@gmail.com>
2023-12-11 17:24:17 +0100

    Your commit message here

---
+++ Array.js
@@ -0,0 +1,15 @@
+const students = [
+  { id: 1, name: "Alice", grade: 85 },
+  { id: 2, name: "Bob", grade: 92 },
+  { id: 3, name: "Charlie", grade: 78 },
+];
+
+function data() {
+  return [
+    { id: 1, name: "Alice", grade: 85 },
+    { id: 2, name: "Bob", grade: 92 },
+    { id: 3, name: "Charlie", grade: 78 },
+  ];
+}
+
+console.log(data());
